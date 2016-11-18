048da55a55fdce4e8ef46111baead5bd4a8a4bc3
Vladimir Nevinniy <vladimir@Mac-mini-Vladimir.local>
2016-11-18 23:51:34 +0200

    ????

--- BubbleTea/AppDelegate.swift
+++ BubbleTea/AppDelegate.swift
@@ -27,8 +27,122 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         
         
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Venue")
+        
+        do {
+            
+            let results =
+                try coreDataStack.context.count(for: fetchRequest)
+            
+            if (results == 0) {
+                
+                do {
+                    let results =
+                        try coreDataStack.context.fetch(fetchRequest) as! [Venue]
+                    
+                    for object in results {
+                        let team = object as Venue
+                        coreDataStack.context.delete(team)
+                    }
+                    
+                    coreDataStack.saveContext()
+                    importJSONSeedData()
+                    
+                } catch let error as NSError {
+                    print("Error fetching: \(error.localizedDescription)")
+                }
+            }
+
+            
+        } catch let error {
+            print(error)
+        }
+        
+        
         return true
     }
+    
+    
+    
+    func importJSONSeedData() {
+        let jsonURL = Bundle.main.url(forResource: "seed", withExtension: "json")
+        let jsonData = try? Data(contentsOf: jsonURL!)
+        
+        let venueEntity = NSEntityDescription.entity(forEntityName: "Venue", in: coreDataStack.context)
+        let locationEntity = NSEntityDescription.entity(forEntityName: "Location", in: coreDataStack.context)
+        let categoryEntity = NSEntityDescription.entity(forEntityName: "Category", in: coreDataStack.context)
+        let priceEntity = NSEntityDescription.entity(forEntityName: "PriceInfo", in: coreDataStack.context)
+        let statsEntity = NSEntityDescription.entity(forEntityName: "Stats", in: coreDataStack.context)
+        
+        do {
+            let jsonDict = try JSONSerialization.jsonObject(with: jsonData!, options: .allowFragments) as! Dictionary<String, Any>
+            
+            
+            let jsonDict2 = jsonDict["response"]  as! Dictionary<String, Any>
+          
+            let jsonArray = jsonDict2["venues"] as! Array<AnyObject>
+                
+            
+            
+            
+            for jsonDictionary in jsonArray {
+                
+                let venueName = jsonDictionary["name"] as? String
+                let venuePhone = (jsonDictionary as AnyObject).value(forKeyPath: "contact.phone") as? String
+                let specialCount = (jsonDictionary as AnyObject).value(forKeyPath: "specials.count") as? NSNumber
+                
+                let locationDict = jsonDictionary["location"] as! NSDictionary
+                let priceDict = jsonDictionary["price"] as! NSDictionary
+                let statsDict = jsonDictionary["stats"] as! NSDictionary
+                
+                let location = Location(entity: locationEntity!, insertInto: coreDataStack.context)
+                location.address = locationDict["address"] as? String
+                location.city = locationDict["city"] as? String
+                location.state = locationDict["state"] as? String
+                location.zipcode = locationDict["postalCode"] as? String
+                location.distance = locationDict["distance"] as? NSNumber as! Float
+                
+                let category = Category(entity: categoryEntity!, insertInto: coreDataStack.context)
+                
+                let priceInfo = PriceInfo(entity: priceEntity!, insertInto: coreDataStack.context)
+                priceInfo.priceCategory = priceDict["currency"] as? String
+                
+                let stats = Stats(entity: statsEntity!, insertInto: coreDataStack.context)
+                stats.checkinsCount = statsDict["checkinsCount"] as? NSNumber as! Int32
+                
+                
+                if let usersCount = statsDict["userCount"] {
+                    stats.usersCount = usersCount as! Int32
+                }
+                
+                
+                if let tipCount = statsDict["tipCount"] {
+                    stats.tipCount =  tipCount as! Int32
+                }
+                
+                let venue = Venue(entity: venueEntity!, insertInto: coreDataStack.context)
+                venue.name = venueName
+                venue.phone = venuePhone
+                
+                venue.specialCount = specialCount as! Int32
+                
+                
+                venue.location = location
+                venue.category = category
+                venue.priceInfo = priceInfo
+                venue.stats = stats
+            }
+            
+            coreDataStack.saveContext()
+        } catch let error as NSError {
+            print("Deserialization error: \(error.localizedDescription)")
+        }
+    }
+    
+
+
+    
+    
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
